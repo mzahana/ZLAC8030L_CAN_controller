4b827768875727f78f8304d7353a305ecb871420
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2022-02-07 16:23:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/mzahana/ZLAC80380L_CAN_controller.git
+# Learn more: https://github.com/mzahana/ZLAC8030L_CAN_controller.git
 # Reference for Python package structuring (https://www.pythonforthelab.com/blog/how-create-setup-file-your-project/)
 
 from setuptools import setup, find_packages
@@ -13,18 +13,18 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='ZLAC80380L_CAN_controller',
+    name='ZLAC8030L_CAN_controller',
     version='0.1.0',
     description='A Python package for CANopen communication with the ZLAC8030L motor driver.',
     long_description=readme,
     author='Mohamed Abdelkader',
     author_email='mohamedashraf123@gmail.com',
-    url='https://github.com/mzahana/ZLAC80380L_CAN_controller.git',
+    url='https://github.com/mzahana/ZLAC8030L_CAN_controller.git',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     entry_points={
         'console_scripts': [
-            'zlac8030l_test= ZLAC80380L_CAN_controller.__main__:main',
+            'zlac8030l_dummy_test=tests.dummy_test:main',
         ]
     },
     install_requires=[
