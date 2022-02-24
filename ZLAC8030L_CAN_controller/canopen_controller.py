6e8ad1f994d2088b2827d50b39192ac1c4e5c7ff
AymanKaram <aiman.karam@gmail.com>
2022-02-24 17:29:48 +0300

    Update canopen_controller.py

--- ZLAC8030L_CAN_controller/canopen_controller.py
+++ ZLAC8030L_CAN_controller/canopen_controller.py
@@ -31,17 +31,17 @@ import traceback
 import time
 import canopen
 from canopen.profiles.p402 import BaseNode402
-from scipy.fftpack import fft
+import time
 #from scipy.fftpack import idstn
 
 # Set logging level. logging.DEBUG will log/print all messages
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 class MotorController:
    """MotorController
    Implements convenience methods to monitor and operate ZLAC8030L drivers
    """
-   def __init__(self, channel='can0', bustype='socketcan', bitrate=500000, node_ids=None, debug=False, eds_file=None):
+   def __init__(self, channel='can0', bustype='socketcan', bitrate=500000, node_ids=None, debug=False, eds_file=None, sync_dt=0.01):
       """
       @brief Creates and connects to CAN bus
 
@@ -73,7 +73,6 @@ class MotorController:
          # This will attempt to read an SDO from nodes 1 - 127
          self._network.scanner.search()
          # We may need to wait a short while here to allow all nodes to respond
-         time.sleep(0.05)
          logging.info('Available nodes: %s', self._network.scanner.nodes)
                   
          if node_ids is not None: # User specified the expected nodes that should be available
@@ -89,36 +88,51 @@ class MotorController:
                if not (node_id in self._network.scanner.nodes):
                   logging.error("Node ID %s is not available in %s",node_id, self._network.scanner.nodes)
                   raise Exception("Node ID {} is not available in {}".format(node_id, self._network.scanner.nodes)) 
+         # Transmit SYNC every 100 ms
+         logging.info("Starting network Sync\n")
+         self._network.sync.start(sync_dt)
          
          # Add CANopen nodes to the network
          # This will add the detect nodes to the network and set them up with corresponding Object Dictionaries
          for node_id in self._network.scanner.nodes:
             node = canopen.BaseNode402(node_id, eds_file) # This assumes that we can use the same eds file for all motor drivers
             self._network.add_node(node)
-            # Reset the network
+            # Reset communication
             node.nmt.state = 'RESET COMMUNICATION'
-            #node.nmt.state = 'RESET'
             node.nmt.wait_for_bootup(15)
-            logging.debug('node {} state) = {}'.format(node_id, node.nmt.state))
-            node.op_mode = 'PROFILED VELOCITY'
-            logging.debug('Node {} op_mode has been set to {}, it should be PROFILED VELOCITY!'.format(node_id, node.op_mode))
+            assert node.nmt.state == 'PRE-OPERATIONAL'
+            
+            logging.info('node {} state = {}'.format(node_id, node.nmt.state))
+            node.load_configuration()
+            # node.setup_402_state_machine() # This is problematic. It messes theings up!!!
+
+            self.setTPDO(node_id=node_id, pdo_id=1)
+            self.setRPDO(node_id=node_id, pdo_id=1)
+            
+
+            logging.info("Setting OPERATIONAL NMT state of node {}".format(node_id))
+            node.nmt.state = 'OPERATIONAL'
+            assert node.nmt.state == 'OPERATIONAL'
+
+            logging.info("Enabling operation mode for node {}".format(node_id))
+            node.state = 'SWITCH ON DISABLED'
+            node.state = 'READY TO SWITCH ON'
+            node.state = 'SWITCHED ON'
+            node.state = 'OPERATION ENABLED'
+            node.tpdo[1].wait_for_reception()
+            logging.info("State of node {} = {}".format(node_id,node.state))
+            
+            logging.info('Device operation mode {}'.format(node.tpdo[1]['Mode of operation display'].phys))
             
       except Exception as e:
          exc_type, exc_obj, exc_tb = sys.exc_info()
          fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
          logging.error('%s %s %s', exc_type, fname, exc_tb.tb_lineno)
          traceback.print_exc()
-      finally:
+
          # Disconnect from CAN bus
-         logging.error('going to exit... stopping...')
-         if self._network:
-            for node_id in self._network:
-               node =self._network[node_id]
-               node.nmt.state = 'PRE-OPERATIONAL'
-               node.nmt.stop_node_guarding()
-               self._network.sync.stop()
-               self._network.disconnect()
-               return
+         self.disconnectNetwork()
+         exit(1)
       
       # Reaching here means that CAN connection went well.
 
@@ -126,7 +140,7 @@ class MotorController:
       """
       Disconnects the entire CAN network
       """
-      logging.error('going to exit... stopping...')
+      logging.info('Disconnecting CAN network...')
       if self._network:
          for node_id in self._network:
             node =self._network[node_id]
@@ -170,6 +184,43 @@ class MotorController:
                logging.error("Setting NMT [PreOperation] failed for Node ID %s",node_id)
                raise Exception("Setting NMT [PreOperation] failed for Node ID {}".format(node_id)) 
             return
+
+   def setTPDO(self, node_id=1, pdo_id=1):
+      """Sets the TPDO mapping"""
+      logging.info("Setting up TPDO {} of node {}".format(pdo_id, node_id))
+      node = self._network[node_id]
+      try:
+         # Read PDO configuration from node
+         node.tpdo.read()
+         
+         # Re-map TxPDO1
+         node.tpdo[pdo_id].clear()
+         node.tpdo[pdo_id].add_variable('StatusWord')
+         node.tpdo[pdo_id].add_variable('Current Speed')
+         node.tpdo[pdo_id].add_variable('Mode of operation display')
+         node.tpdo[pdo_id].trans_type = 1
+         node.tpdo[pdo_id].event_timer = 0
+         node.tpdo[pdo_id].enabled = True
+         # Save new PDO configuration to node
+         node.tpdo.save() # node must be in PRE-OPERATIONAL NMT state
+      except Exception as e:
+         logging.error("Could not configure TPDO {} for node {}".format(pdo_id, node_id))
+
+   def setRPDO(self, node_id=1, pdo_id=1):
+      """Sets the RPDO mapping"""
+      logging.info("Setting up RPDO {} of node {}".format(pdo_id, node_id))
+      node = self._network[node_id]
+      try:
+         node.rpdo.read()
+         node.rpdo[pdo_id].clear()
+         node.rpdo[pdo_id].add_variable('Target speed')
+         node.rpdo[pdo_id].enabled = True
+         node.rpdo.save()
+
+         node.rpdo[pdo_id]['Target speed'].phys = 0.0
+         node.rpdo[pdo_id].start(0.1)
+      except Exception as e:
+         logging.error("Could not configure RPDO {} for node {}".format(pdo_id, node_id))
 
    def enableOperation(self, node_ids):
       """
@@ -238,24 +289,28 @@ class MotorController:
 
    def _checkNodeID(self, node_id):
       if not type(node_id) is int:
-         raise Exception("[nodeID_sanity_checks] Node ID {} is not int".format(id))
+         raise Exception("[nodeID_sanity_checks] Node ID {} is not int".format(node_id))
       if node_id < 0 or node_id > 127:
-         logging.error("Node ID %s is not in the range [0,127]", id) 
-         raise Exception("Node ID {} is not in the range [0,127]".format(id))
-      if not node_id in self._node_ids:
-         raise Exception("Node ID {} is not available!".format(id))
+         logging.error("Node ID %s is not in the range [0,127]", node_id) 
+         raise Exception("Node ID {} is not in the range [0,127]".format(node_id))
+      if not self._node_ids==None:
+         if not node_id in self._node_ids:
+            raise Exception("Node ID {} is not available!".format(node_id))
         
 
-   def setVelocity(self, node_id):
+   def setVelocity(self, node_id=1, vel=10.0):
       """
       Sets velocity value of a particular node
 
       Parameters
       --
       @param node_id Node ID [int]
+      @param vel Velocity PRM
       """
       # Sanity checks
       self._checkNodeID(node_id)
+      node = self._network[node_id]
+      node.rpdo[1]['Target speed'].phys = vel
 
 
 
@@ -274,8 +329,10 @@ class MotorController:
       # Sanity checks
       # TODO Needs implementation
       #actual_speed = self._network[node_id].sdo['Current Speed']
-      
-      pass
+      node = self._network[node_id]
+      node.tpdo[1].wait_for_reception()
+      speed = node.tpdo[1]['Current Speed'].phys
+      return speed
 
    def getEncoder(self, node_id):
       """
@@ -304,5 +361,9 @@ class MotorController:
       """
       # Sanity checks
       # TODO Needs implementation
+      pass
+
+   def EStop(self):
+      """Emergency STOP"""
       pass
    
